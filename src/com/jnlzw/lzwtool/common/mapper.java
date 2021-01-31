376c5970a19e8e46848cb6531da0e7ebd08fb775
jn_lzw <1481036479@qq.com>
2021-02-01 02:19:08 +0800

    add mapper

--- src/com/jnlzw/lzwtool/common/mapper.java
+++ src/com/jnlzw/lzwtool/common/mapper.java
@@ -2,7 +2,6 @@ package com.jnlzw.lzwtool.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.BeanUtils;
 
 import java.io.ByteArrayInputStream;
@@ -11,12 +10,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -77,67 +71,7 @@ public class mapper {
     }
 
 
-    /**
-     * Object转成指定的类型
-     */
-    public static <T> T convert(Object obj, Class<T> type) {
-        if (obj != null && StringUtils.isNotBlank(obj.toString())) {
-            if (type.equals(Integer.class) || type.equals(int.class)) {
-                return (T) new Integer(obj.toString());
-            } else if (type.equals(Long.class) || type.equals(long.class)) {
-                return (T) new Long(obj.toString());
-            } else if (type.equals(Boolean.class) || type.equals(boolean.class)) {
-                return (T) new Boolean(obj.toString());
-            } else if (type.equals(Short.class) || type.equals(short.class)) {
-                return (T) new Short(obj.toString());
-            } else if (type.equals(Float.class) || type.equals(float.class)) {
-                return (T) new Float(obj.toString());
-            } else if (type.equals(Double.class) || type.equals(double.class)) {
-                return (T) new Double(obj.toString());
-            } else if (type.equals(Byte.class) || type.equals(byte.class)) {
-                return (T) new Byte(obj.toString());
-            } else if (type.equals(Character.class) || type.equals(char.class)) {
-                return (T) new Character(obj.toString().charAt(0));
-            } else if (type.equals(String.class)) {
-                return (T) obj;
-            } else if (type.equals(BigDecimal.class)) {
-                return (T) new BigDecimal(obj.toString());
-            } else if (type.equals(LocalDateTime.class)) {
-                //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                return (T) LocalDateTime.parse(obj.toString());
-            } else if (type.equals(Date.class)) {
-                try {
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                    return (T) formatter.parse(obj.toString());
-                } catch (ParseException e) {
-                    throw new RuntimeException(e.getMessage());
-                }
 
-            } else {
-                return null;
-            }
-        } else {
-            if (type.equals(int.class)) {
-                return (T) new Integer(0);
-            } else if (type.equals(long.class)) {
-                return (T) new Long(0L);
-            } else if (type.equals(boolean.class)) {
-                return (T) new Boolean(false);
-            } else if (type.equals(short.class)) {
-                return (T) new Short("0");
-            } else if (type.equals(float.class)) {
-                return (T) new Float(0.0);
-            } else if (type.equals(double.class)) {
-                return (T) new Double(0.0);
-            } else if (type.equals(byte.class)) {
-                return (T) new Byte("0");
-            } else if (type.equals(char.class)) {
-                return (T) new Character('\u0000');
-            } else {
-                return null;
-            }
-        }
-    }
 
     /**
      * @param key    属性名
@@ -207,25 +141,25 @@ public class mapper {
         return null;
     }
 
+    /**
+     *
+     * @param 原对象
+     * @return 深拷贝对象
+     */
     public static Object copy(Object oldObj) {
         Object obj = null;
         try {
-            // Write the object out to a byte array
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ObjectOutputStream out = new ObjectOutputStream(bos);
             out.writeObject(oldObj);
             out.flush();
             out.close();
 
-            // Retrieve an input stream from the byte array and read
-            // a copy of the object back in.
             ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
             ObjectInputStream in = new ObjectInputStream(bis);
             obj = in.readObject();
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException cnfe) {
-            cnfe.printStackTrace();
         }
         return obj;
     }
